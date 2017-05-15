996b8bb1e443f551f2ad90a8a6f720511751b2c9
KsCorrales <kscorrales@hotmail.com>
2017-05-15 16:17:48 -0500

    terminos actualizados

--- LARAVEL/app/views/terminos-concurso.blade.php
+++ LARAVEL/app/views/terminos-concurso.blade.php
@@ -37,7 +37,296 @@
         </div>
 
         <div class="container pm-containerPadding80">
-
+            <p align="center">
+                <strong>CONCURSO “A GANAR PARA MAMÁ”</strong>
+            </p>
+            <p>
+                Para celebrar el día de la madre Café Águila Roja plantea un concurso
+                dirigido a mujeres y hombres mayores de edad, residentes en Colombia.
+            </p>
+            <br>
+            <p>
+                <strong>I. Medios de difusión:</strong>
+            </p>
+            <p>
+                La promoción del concurso será difundida a través de:
+            </p>
+            <p>
+                • Se promocionará en las redes sociales (Facebook e Instagram) de la marca
+                a través de una publicación.
+            </p>
+            <br>
+            <p>
+                <strong>II Participantes: </strong>
+            </p>
+            <p>
+                Podrán participar hombres y mujeres mayores de edad, con residencia en
+                Colombia, que cumplan con los requisitos descritos en la mecánica del reto
+                y que lean, comprendan y acepten los términos y condiciones aquí
+                contenidos.
+            </p>
+            <br>
+            <p>
+                <strong>
+                    Quedan expresamente excluidos de participar en el concurso las
+                    siguientes personas:
+                </strong>
+            </p>
+            <p>
+                Los empleados de Café Águila Roja, sus cónyuges y familiares hasta el
+                segundo grado de consanguinidad, segundo de afinidad y único civil.
+            </p>
+            <p>
+                Los empleados y trabajadores de las compañías proveedoras de insumos
+                relacionados con los productos participantes, sus cónyuges y familiares
+                hasta el segundo grado de consanguinidad, segundo de afinidad y único
+                civil.
+            </p>
+            <p>
+                Los empleados y trabajadores de las agencias de publicidad e internet
+                intervinientes en la actividad promocional, sus cónyuges y familiares hasta
+                el segundo grado de consanguinidad, segundo de afinidad y único civil.
+            </p>
+            <br>
+            <p>
+                <strong>III. Mecánica promocional: </strong>
+            </p>
+            <p>
+                El concurso <strong>A GANAR PARA MAMÁ</strong>, se llevará a cabo en todo
+                el territorio Colombiano durante el mes de mayo del lunes 15 al 27 de Mayo
+                de 2017. Hora de inicio 8:00 AM 15 de Mayo, hora de finalización a las 6:00
+                PM, viernes 27 de Mayo de 2017.
+            </p>
+            <p>
+                Para participar en la presente actividad promocional, los usuarios deberán:
+            </p>
+            <p>
+                Ingresar al fan page de Café Águila Roja en Facebook
+                <a href="https://www.facebook.com/CafeAguilaRoja/">
+                    https://www.facebook.com/CafeAguilaRoja/
+                </a>
+                y darle “me gusta” o “like” a la página.
+            </p>
+            <p>
+                Se publicará 1 post explicando la mecánica de la actividad, la cual el
+                participante deberá realizar según las instrucciones dadas.
+            </p>
+            <p>
+                El reto de la actividad es el siguiente:
+            </p>
+            <br>
+            <p>
+                <strong>Mi mamá merece lo mejor con Águila Roja</strong>
+            </p>
+            <p>
+                · Los participantes deben grabar un video de 30 o 40 segundos conmovedor y
+                emotivo contándonos porque su mamá es merecedora de una cafetera Águila
+                Roja. Luego debe publicarlo como comentario en la parte inferior de la
+                publicación usando el hashtag #UnCaféConMamá #MamáYSuCafé
+            </p>
+            <p>
+                · Los 7 videos más conmovedores, creativos y emotivos ganan una cafetera.
+            </p>
+            <p>
+                · Solo se podrá publicar un video por participante.
+            </p>
+            <p>
+                · Válido hasta el 27 Mayo de 2017 a las 6:00 pm.
+            </p>
+            <p>
+                *Café Águila Roja se reserva la autonomía para escoger bajo su criterio a
+                los ganadores de del concurso <strong>A GANAR PARA MAMÁ</strong>.
+            </p>
+            <p>
+                Los criterios en caso de descalificación de los participantes son los
+                siguientes:
+            </p>
+            <p>
+                § Cualquier sospecha de fraude o suplantación con perfiles de Facebook
+                falsos.
+            </p>
+            <p>
+                § Personas que no acepten los términos o condiciones de la actividad.
+            </p>
+            <p>
+                § Para la valoración del material participante (videos), se tendrá en
+                cuenta que dichos elementos no contengan ningún tipo de material que haya
+                puesto en riesgo, real o potencialmente, la integridad o detrimento de la
+                imagen de la marca (Café Águila Roja), ni de cualquier otro ser vivo, ni de
+                propiedad material o intelectual de terceros.
+            </p>
+            <p>
+                § Videos con claras evidencias de montaje.
+            </p>
+            <p>
+                § Videos que no cumplan con los requerimientos del concurso.
+            </p>
+            <br>
+            <p>
+                <strong>V. Premios que la compañía Café Águila Roja entregará:</strong>
+            </p>
+            <p>
+                · Café Águila Roja hará entrega en total de siete (7) cafeteras, premiando
+                en total a 7 personas.
+            </p>
+            <p>
+                · El premio no puede cederse, debe ser reclamado sólo por el titular mayor
+                de edad del perfil participante, no son acumulables con otras promociones.
+                Los premios ofrecidos no serán canjeables, bajo ninguna circunstancia, por
+                dinero en efectivo. Además, el ganador que no pueda recibir su premio no
+                tendrá derecho a compensación alguna. Para reclamar los premios los
+                ganadores tienen un plazo máximo de 15 días hábiles después de ser
+                anunciados.
+            </p>
+            <br>
+            <p>
+                <strong>VI. Forma de contacto con los ganadores y entrega de premios</strong>
+            </p>
+            <p>
+                El modo de entrega de los premios se hará de la siguiente manera:
+            </p>
+            <p>
+                Se informará a los ganadores del concurso    <strong>A GANAR PARA MAMÁ</strong>:
+            </p>
+            <p>
+                · A través de mensaje interno en Facebook, solicitándoles el envío de sus
+                datos personales (nombre completo, documento de identidad, ciudad,
+                dirección, teléfono y correo electrónico).
+            </p>
+            <p>
+                · A través de mensajes privados se les confirmará a los ganadores que sus
+                datos han sido recibidos.
+            </p>
+            <p>
+                · A través de publicaciones en la página de Facebook anunciando que es uno
+                de los ganadores.
+            </p>
+            <p>
+                · Se contactará a los ganadores vía telefónica para la entrega del premio
+                con un representante de Café Águila Roja o un correo certificado el cual
+                coordinará la fecha y hora de envío al lugar designado por el participante
+                en un lapso de 2 semanas o más según el lugar de residencia.
+            </p>
+            <br>
+            <p>
+                <strong>VII. Planilla de entrega de premios:</strong>
+            </p>
+            <p>
+                Todo ganador deberá firmar una planilla que certifica la entrega del premio
+                y su recibo a satisfacción; además, deberá presentar su cédula de
+                ciudadanía original. Sin el cumplimiento de la totalidad de lo
+                anteriormente descrito, el ganador no podrá recibir su premio.
+            </p>
+            <br>
+            <p>
+                <strong>VIII. Autorización tratamiento de datos personales</strong>
+            </p>
+            <p>
+                El usuario autoriza a Café Águila Roja (o cualquier otra compañía que sea
+                contratada para las mismas finalidades) (en adelante Responsables y
+                Encargados) a tratar los datos personales que en desarrollo del concurso “    <strong>A GANAR PARA MAMÁ</strong>” se obtengan, con la finalidad de
+                contactar a los ganadores, hacer entrega del premio y, en general, todo lo
+                necesario para el desarrollo de la actividad.
+            </p>
+            <p>
+                Los datos personales que los usuarios suministren en desarrollo del
+                concurso serán confidenciales, no pudiendo los Responsables o Encargados
+                revelarlos a terceros ni utilizar la información para fines distintos de
+                los autorizados por el propio usuario. Los datos personales permanecerán en
+                entornos operativos seguros no disponibles al público.
+            </p>
+            <p>
+                Se le informa al usuario que como titular de los datos personales, le
+                asisten los siguientes derechos:
+            </p>
+            <p>
+                a) Conocer, actualizar y rectificar sus datos personales frente a los
+                Responsables o Encargados.
+            </p>
+            <p>
+                b) Solicitar prueba de la autorización otorgada a los Responsables o
+                Encargados.
+            </p>
+            <p>
+                c) Ser informado por los Responsables o Encargados, previa solicitud,
+                respecto del uso que le ha dado a sus datos personales.
+            </p>
+            <p>
+                d) Previa queja o consulta ante los Responsables o Encargados, presentar
+                ante la Superintendencia de Industria y Comercio quejas por infracciones a
+                la normatividad legal aplicable.
+            </p>
+            <p>
+                e) Revocar la autorización y/o solicitar la supresión del dato cuando en el
+                Tratamiento no se respeten los principios, derechos y garantías
+                constitucionales y legales.
+            </p>
+            <p>
+                f) Acceder en forma gratuita a sus datos personales que hayan sido objeto
+                de Tratamiento.
+            </p>
+            <br>
+            <p>
+                <strong>IX. Modificación, cancelación o suspensión del concurso</strong>
+            </p>
+            <p>
+                Café Águila Roja podrá a su solo criterio: (a) ampliar o disminuir la
+                nómina de sorteos y premios establecida en estas bases o reemplazarlos por
+                otros; (b) modificar las fechas de comienzo y finalización de la promoción;
+                (c) modificar estas bases; (d) sí el concurso no cumple con la convocatoria
+                Café Águila Roja podrá desistir de continuar con el concurso comunicándolo
+                en forma masiva, sin que ello otorgue derecho alguno a los participantes.
+            </p>
+            <p>
+                Cualquier tipo de fraude o de intento del mismo, al igual que cualquier
+                incumplimiento de los términos y condiciones aquí descritas, dará lugar a
+                que el participante sea descalificado del concurso, sin posibilidad de
+                poder reingresar a este. Café Águila Roja podrá verificar el fraude, el
+                intento de fraude o el incumplimiento, por cualquier medio que considere
+                pertinente.
+            </p>
+            <br>
+            <p>
+                <strong>IX. Varios</strong>
+            </p>
+            <p>
+                Las ideas, opiniones, sugerencias y comentarios que sean enviados por los
+                concursantes a Café Águila Roja, en forma espontánea y sin previa
+                solicitud, podrán ser utilizados por esta. Lo anterior, bajo el
+                entendimiento de que tales comunicaciones no son confidenciales y no están
+                protegidas por ninguna regulación relacionada con temas de propiedad
+                intelectual o industrial. Por ende, el remitente de tal información no
+                podrá reclamar indemnización o participación alguna en razón del uso
+                comercial o extracomercial que la referida compañía haga de la información
+                en cuestión.
+            </p>
+            <p>
+                Café Águila Roja no será responsable por ningún daño o perjuicio que
+                sufran, directa o indirectamente, y en conexión con la realización de esta
+                actividad promocional, o con los premios ofrecidos dentro de esta, los
+                participantes, los ganadores y/o terceras personas, salvo que dicho daño o
+                perjuicio le sea imputable a la referida compañía.
+            </p>
+            <p>
+                Este documento, y todas las relaciones de cualquier tipo que se deriven de
+                él, se regirán por la ley de la República de Colombia.
+            </p>
+            <p>
+                Las marcas, nombres comerciales, gráficos, dibujos, diseños y cualquier
+                otra figura que constituya propiedad intelectual o industrial y que
+                aparezca en el sitio web, están protegidos a favor de Café Águila Roja, de
+                conformidad con las disposiciones legales sobre la materia. En
+                consecuencia, los elementos aquí referidos no podrán ser utilizados,
+                modificados, copiados, reproducidos, transmitidos o distribuidos de ninguna
+                manera y por ningún medio, salvo autorización previa, escrita y expresa de
+                la compañía.
+            </p>
+            <p>
+                Los contenidos protegidos de conformidad con el párrafo anterior, incluyen
+                textos, imágenes, ilustraciones, dibujos, diseños, software, música,
+                sonido, fotografías y videos, además de cualquier otro medio o forma de
+                difusión de dichos contenidos.
+            </p>
         </div>
 
         </div>
